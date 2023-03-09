8985d84a6657db25e0a9a0f0717885c0154d46da
Spade <47337405+SagarPateI@users.noreply.github.com>
2023-03-09 15:59:39 -0500

    Testing changes

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 // Get the button element
 var button = document.getElementById("button2");
+//call button inside the function
 
 // Listen for the mouse position
 document.addEventListener("mousemove", function (event) {
+ 
     // Get the current position of the button
     // var x = parseInt(window.getComputedStyle(button).getPropertyValue("left"));
     // var y = parseInt(window.getComputedStyle(button).getPropertyValue("top"));
-
+    var currentTransformation = String(button.style.transform);
+    console.log(currentTransformation);
     var transformValue = window.getComputedStyle(button).getPropertyValue("transform");
+    console.log(transformValue)
     var xyValues = transformValue.match(/-?\d+\.?\d*/g);
     var x = parseInt(xyValues[4]);
     var y = parseInt(xyValues[5]); 
+    console.log(x, y)
 
 
     // Calculate the distance between the button and the cursor
     var distance = Math.sqrt(Math.pow((x - event.clientX), 2) + Math.pow((y - event.clientY), 2));
-
+    console.log(distance)
     // If the distance is less than 150 pixels, move the button away from the cursor
     if (distance < 150) {
         // Calculate the new position of the button
@@ -25,7 +30,7 @@ document.addEventListener("mousemove", function (event) {
         // Update the position of the button
         // button.style.left = newX + "px";
         // button.style.top = newY + "px";
-        button.style.transform = 'translateX(${newX}px) translateY(${newY}px)';
+        button.style.transform = 'translateX(${newX}%) translateY(${newY}%)';
     }
 });
 
